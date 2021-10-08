6fab054537cb0d66fc43db9950fefcebd2908108
Ingrid <ingrid7613@gmail.com>
2021-10-08 10:33:35 +0800

    Deploy v1.0.3 - modify mapbox token

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,12 @@ set -e
 npm run build
 
 # 移動至到打包後的dist目錄 
-cd dist
+# cd dist
 
 # 因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 # git init 
 git add -A
-git commit -m 'Deploy v1.0.1'
+git commit -m 'Deploy v1.0.3 - modify mapbox token'
 
 # 將 dist資料夾中的內容推送至遠端 ScrollStoryMapBox main分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 git push -f https://github.com/ingridkao/ScrollStoryMapBox.git master:github-pages
